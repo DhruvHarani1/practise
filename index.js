a5c8f316f983ded1f384f2d16220ac3ad9758cd4
DhruvHarani1 <dhruvharani5@gmail.com>
2026-04-21 10:14:35 +0530

    added some intital html

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'frontend')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`server running at http://localhost:${port}`);
+});
